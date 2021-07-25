a3e1c893636dee9452ed93ac62a445bb2341d339
Jesus Soto <tonysotogomez@gmail.com>
2021-07-25 00:09:35 -0500

    endpoint correo

--- endpoint.php
+++ endpoint.php
@@ -2,22 +2,28 @@
 
 if ( $_REQUEST ) {
     if (isset($_REQUEST['id'])) {
-        $file = fopen("notificacion_".$_REQUEST['id'].".txt", "w");
+        $var = "";
+        //$file = fopen("notificacion_".$_REQUEST['id'].".txt", "w");
         foreach ( $_REQUEST as $key => $value ) {
-            $var = $key.": ".$value;
-            fwrite($file, $var. PHP_EOL);
-            header("HTTP/1.1 200 OK");
+            $var .= $key.": ".$value. "\n";
+            //fwrite($file, $var. PHP_EOL);
         }
+        $to = "tonysotogomez@email.com";
+        $subject = "MP Endpoint";
+        $message = json_encode($var);
+        
+        mail($to, $subject, $message);
+        header("HTTP/1.1 200 OK");
     } else {
-        $file = fopen("notificacion_incorrecta.txt", "w");
-        fwrite($file, "". PHP_EOL);
+        //$file = fopen("notificacion_incorrecta.txt", "w");
+        //fwrite($file, "". PHP_EOL);
     }
     
 } else {
-    $file = fopen("notificacion_no_recibida.txt", "w");
-    fwrite($file, "". PHP_EOL);
+    //$file = fopen("notificacion_no_recibida.txt", "w");
+    //fwrite($file, "". PHP_EOL);
 }
-fclose($file);
+//fclose($file);
 
 
 ?>
